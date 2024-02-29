5c1623d7997ab30828557fbd3f83469e859eaeec
ZinhZinh <22211TT2017@nhc.edu.vn>
2024-02-29 10:54:25 +0700

    Sua lan 2

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
 Route::get('/page1',function(){
     return view('page1');
 });
-// Route::get('/page2',function(){
-//     return view('page1');
-// });
+Route::get('/page2',function(){
+     return view('page1');
+});
 Route::get('/page2',function(){
     return view('page2');
 });
