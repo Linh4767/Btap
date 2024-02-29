d5c989fad9d845cf1289ed9b43ae2b65223eb32d
trieu22211tt0613 <22211tt0613@mail.tdc.edu.vn>
2024-02-29 10:26:08 +0700

    Linh4797

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/page1',function(){
-    return view('pag
-    e1');
+    return view('page1');
+});
+Route::get('/page2',function(){
+    return view('page1');
 });
 Route::get('/page2',function(){
     return view('page1');
