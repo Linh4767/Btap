7a636c69b745bc0c47172f56a5cb1317e216bbf4
tantai130507 <td130507@gmail.com>
2024-03-21 09:07:58 +0700

    Lan 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\SignUp;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 // Route::get('admin/{age}', function(string $age){
 //     return "Ban du tuoi truy cap trang nay";
 // })->whereNumber('age')->middleware('checkage');
+
+Route::get('/', [SignUp::class, 'index']);
+Route::get('/signup', [SignUp::class, 'signup']);
+Route::post('process', [SignUp::class, 'process']);
 
 
 Route::post('/login', function (){
